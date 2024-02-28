5421292e483243d080da1a5fed6bbf777b8e5dde
MinseoKangQ <kms02171@naver.com>
2024-02-28 18:31:45 +0900

    :sparkles: GlobalExceptionHandler 클래스 추가

---
+++ src/main/java/kr/go/data/util/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package kr.go.data.util.exception;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import kr.go.data.util.response.CustomApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomApiResponse<?>> handleMethodArgumentNotValidationException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CustomApiResponse.createFailWithoutData(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<CustomApiResponse<?>> handleConstraintViolationException(ConstraintViolationException e) {
+        String errorMessage = e.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("; "));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CustomApiResponse.createFailWithoutData(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+
+    // Custom Exception
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<CustomApiResponse<?>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(CustomApiResponse.createFailWithoutData(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(EntityDuplicatedException.class)
+    public ResponseEntity<CustomApiResponse<?>> handleEntityDuplicatedException(EntityDuplicatedException e) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(CustomApiResponse.createFailWithoutData(HttpStatus.CONFLICT.value(), e.getMessage()));
+    }
+
+
+}
