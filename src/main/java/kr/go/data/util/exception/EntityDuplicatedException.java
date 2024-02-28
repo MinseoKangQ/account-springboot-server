9bdf50828a185ee5d206c2aa0f643036c1d7523c
MinseoKangQ <kms02171@naver.com>
2024-02-28 18:30:58 +0900

    :sparkles: EntityDuplicatedException 클래스 추가

---
+++ src/main/java/kr/go/data/util/exception/EntityDuplicatedException.java
@@ -0,0 +1,7 @@
+package kr.go.data.util.exception;
+
+public class EntityDuplicatedException extends RuntimeException{
+    public EntityDuplicatedException(String message) {
+        super(message);
+    }
+}
