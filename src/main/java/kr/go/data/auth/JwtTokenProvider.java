e2ddb6f4d51d0fb73495f9e48f185fe339afd027
MinseoKangQ <kms02171@naver.com>
2024-04-07 00:48:30 +0900

    :sparkles: JwtTokenProvider 클래스 작성
    Jwt 토큰을 생성하고 검증하는 서비스 구현, 사용자가 로그인 시 토큰을 생성하고, 이후 API 요청의 헤더에 포함된 토큰을 검증하는 기능을 제공"

--- src/main/java/kr/go/data/auth/JwtTokenProvider.java
+++ src/main/java/kr/go/data/auth/JwtTokenProvider.java
@@ -1,5 +1,9 @@
 package kr.go.data.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +12,25 @@ public class JwtTokenProvider {
 
     @Value("${jwt.secretKey}")
     private String secretKey;
+
+    // 토큰 생성
+    public String createToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 36000000); // 1시간 유효
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        Jwts.parser().setSigningKey(secretKey).parseClaimsJwt(token);
+        return true;
+    }
 
 }
