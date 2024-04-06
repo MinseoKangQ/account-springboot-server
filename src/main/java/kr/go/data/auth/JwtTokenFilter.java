64a5ad09de69c164de9d93730d731da1004604fa
MinseoKangQ <kms02171@naver.com>
2024-04-07 02:50:32 +0900

    :recycle: JwtTokenFilter 클래스 리팩토링

--- src/main/java/kr/go/data/auth/JwtTokenFilter.java
+++ src/main/java/kr/go/data/auth/JwtTokenFilter.java
@@ -9,8 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -26,33 +24,35 @@ public class JwtTokenFilter extends GenericFilterBean {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String token = request.getHeader("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
-            token = token.substring(7); // "Bearer " 접두어 제거
-            if (jwtTokenProvider.validateToken(token)) {
-                // 토큰에서 권한 정보 추출
-                Claims claims = jwtTokenProvider.getClaimsFromToken(token);
-                List<SimpleGrantedAuthority> authorities = null;
+        String token = extractToken(request);
 
-                if (claims.get("roles") != null) {
-                    String rolesStr = claims.get("roles").toString();
-                    authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + rolesStr));
-                } else {
-                    // "roles"가 없을 경우 기본 권한 설정 가능
-                    authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-                }
-
-                // Spring Security Authentication 객체 생성 및 설정
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                        claims.getSubject(), null, authorities);
-                SecurityContextHolder.getContext().setAuthentication(auth);
-
-                filterChain.doFilter(servletRequest, servletResponse);
-                return;
-            }
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            setSecurityContext(token);
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            unauthorizedResponse(servletResponse);
         }
+    }
 
-        // 토큰이 유효하지 않을 경우 또는 토큰이 없을 경우
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private void setSecurityContext(String token) {
+        Claims claims = jwtTokenProvider.getClaimsFromToken(token);
+        String rolesStr = claims.get("roles", String.class);
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + rolesStr);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                claims.getSubject(), null, Collections.singletonList(authority));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    private void unauthorizedResponse(ServletResponse servletResponse) throws IOException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
